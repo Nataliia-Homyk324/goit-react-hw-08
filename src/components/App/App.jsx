38cc119c411ffc2b7ea027dc47d59480f02e75f4
Nataliia-Homyk324 <homyk324@gmail.com>
2024-05-15 21:43:54 +0300

    RestrictedRoute

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,8 @@ import Layout from '../Layout/Layout';
 import { refreshUser } from '../../redux/auth/operations';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectIsRefreshing } from '../../redux/auth/selectors';
+import RestrictedRoute from '../RestrictedRoute';
+// import PrivateRoute from './PrivateRoute';
 
 const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
 const RegisterPage = lazy(() =>
@@ -29,8 +31,24 @@ export default function App() {
       <Suspense fallback={null}>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute component={<RegisterPage />} redirectTo="/" />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute
+                component={<LoginPage />}
+                redirectTo="/contacts"
+              />
+            }
+          />
+
+          {/* <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} /> */}
 
           <Route path="/contacts" element={<ContactsPage />} />
         </Routes>
